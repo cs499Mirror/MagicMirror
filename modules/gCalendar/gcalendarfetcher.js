3cc979b74c674bf5e82148d1167e2c860bd363ed
Jared Green <greenj859@gmail.com>
2017-04-06 13:21:10 -0400

    moved quickstart code to gcalendarfetcher. Having an issue with google auth api...

--- modules/gCalendar/gcalendarfetcher.js
+++ modules/gCalendar/gcalendarfetcher.js
@@ -7,7 +7,7 @@
 
 var ical = require("./vendor/ical.js");
 var moment = require("moment");
-var require('fs');
+var fs = require('fs');
 var readline = require('readline');
 var google = require('googleapis');              // Required for google calendar api calls
 var googleAuth = require('google-auth-library'); // Required for oauth token maintenence
@@ -18,6 +18,9 @@ var SCOPES = ['https://www.googleapis.com/auth/calendar.readonly'];
 var TOKEN_DIR = (process.env.HOME || process.env.HOMEPATH ||
     process.env.USERPROFILE) + '/.credentials/';
 var TOKEN_PATH = TOKEN_DIR + 'calendar-nodejs-quickstart.json';
+
+// Google Oauth token
+var auth = [];
 
 // Load user's client_secret.js file
 fs.readFile('modules/gCalendar/quickstart/client_secret.json', function processClientSecrets(err, content) {
@@ -44,7 +47,7 @@ function authorize(credentials, callback) {
   var clientSecret = credentials.installed.client_secret;
   var clientId = credentials.installed.client_id;
   var redirectUrl = credentials.installed.redirect_uris[0];
-  var auth = new googleAuth();
+  auth = new googleAuth();
   var oauth2Client = new auth.OAuth2(clientId, clientSecret, redirectUrl);
 
   // Check if we have previously stored a token.
@@ -140,7 +143,7 @@ function listEvents(auth) {
     if (events.length == 0) {
       console.log('No upcoming events found.');
     } else {
-      console.log('Upcoming 10 events:');
+      console.log('Upcoming 10 events in fetcher:');
       for (var i = 0; i < events.length; i++) {
         var event = events[i];
         var start = event.start.dateTime || event.start.date;
@@ -150,6 +153,42 @@ function listEvents(auth) {
   });
 }
 
+/**
+ * Modified From Google Calendar API documentation
+ * https://developers.google.com/google-apps/calendar/quickstart/nodejs 
+ *
+ * Lists the next 10 events on the user's primary calendar.
+ *
+ * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
+ */
+function googleEvents(auth) {
+  var calendar = google.calendar('v3');
+  calendar.events.list({
+    auth: auth,
+    calendarId: 'primary',
+    timeMin: (new Date()).toISOString(),
+    maxResults: 10,
+    singleEvents: true,
+    orderBy: 'startTime',
+  }, function(err, response) {
+    if (err) {
+      console.log('The API returned an error: ' + err);
+      return;
+    }
+    var events = response.items;
+    if (events.length == 0) {
+      console.log('No upcoming events found.');
+    } else {
+      console.log('Upcoming 10 events:');
+      for (var i = 0; i < events.length; i++) {
+        var event = events[i];
+        var start = event.start.dateTime || event.start.date;
+    console.log('%s - %s', start, event.summary);
+      }
+    }
+  });
+  return calendar; 
+}
 
 var CalendarFetcher = function(url, reloadInterval, maximumEntries, maximumNumberOfDays, user, pass) {
 	var self = this;
@@ -189,7 +228,7 @@ var CalendarFetcher = function(url, reloadInterval, maximumEntries, maximumNumbe
 	*/
 		if (url === 'https://www.googleapis.com/auth/calendar') {
 			console.log("..in gcalendarfetcher, here's current cal");
-//			quickstart;
+			listEvents(auth);	
 		}
 		ical.fromURL(url, opts, function(err, data) {
 			if (err) {
